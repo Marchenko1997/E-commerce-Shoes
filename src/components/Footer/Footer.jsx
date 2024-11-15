badb6c467ff66f23a8dceda32fbbca255051a00c
Halyna Marchenko <marchenkohalyna888@gmail.com>
2024-11-15 18:32:35 +0100

    creating Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import { Box, Text } from "@radix-ui/themes";
+import css from "./Footer.module.css";
 
-const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
+export default function Footer() {
+    return (
+      <footer className={css.footer}>
+        <Box className={css.footerContainer}>
+          <Text as="p" size="2" style={{ color: "var(--accent-10)" }}>
+            2024 | All images are the property of adidas.
+          </Text>
+        </Box>
+      </footer>
+    );
 }
-
-export default Footer
