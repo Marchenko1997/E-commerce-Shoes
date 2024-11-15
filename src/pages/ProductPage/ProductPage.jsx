75ce2764f424b191dc3cde832ef2eb2afc174932
Halyna Marchenko <marchenkohalyna888@gmail.com>
2024-11-15 17:20:06 +0100

    creating SelectColor

--- src/pages/ProductPage/ProductPage.jsx
+++ src/pages/ProductPage/ProductPage.jsx
@@ -1,12 +1,14 @@
 import { products } from "../../assets/db/products_list"
 import AboutProduct from "../../components/AboutProduct/AboutProduct.jsx";
 import ProductAccordion from "../../components/ProductAccordion/ProductAccordion.jsx";
+import Slider from "../../components/Slider/Slider.jsx";
 
 const product = products[0];
 
 const ProductPage = () => {
   return (
       <div>
+        <Slider product={product} curentColor="white"/>
           <ProductAccordion product={product}/>
         <AboutProduct product={product}/>
     </div>
