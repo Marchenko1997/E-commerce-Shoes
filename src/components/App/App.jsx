6303250a3e524f7417eab2b68044399cfda6620b
Halyna Marchenko <marchenkohalyna888@gmail.com>
2024-11-15 18:37:25 +0100

    creating CartPage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,12 +3,14 @@ import { lazy } from "react";
 import Layout from "../Layout/Layout";
 
 const ProductPage = lazy(() => import("../../pages/ProductPage/ProductPage"));
+const CartPage = lazy(() => import("../../pages/CartPage/CartPage"));
 
 const App = () => {
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<ProductPage />} />
+        <Route path="/cart" element={<CartPage/>}/>
       </Routes>
     </Layout>
   );
