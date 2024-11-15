92e733930b778daafc9e1fc976b8637d0c65bdc2
Halyna Marchenko <marchenkohalyna888@gmail.com>
2024-11-15 14:59:35 +0100

    creating AboutItem

--- src/components/AboutItem/AboutItem.jsx
+++ src/components/AboutItem/AboutItem.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { Box, Flex, Heading, Text } from "@radix-ui/themes";
+import { useMedia } from "react-use";
+import css from "./AboutItem.module.css";
 
-const AboutItem = () => {
+export default function About({ title, description, image, reverse }) {
+  const isTablet = useMedia("(min-width: 768px)");
+
   return (
-    <div>AboutItem</div>
-  )
+    <Flex
+      direction={
+        isTablet ? (reverse ? "row-reverse" : "row") : "column-reverse"
+      }
+    >
+      <Flex
+        className={css.content}
+        direction="column"
+        align="center"
+        justify="center"
+      >
+        <Box maxWidth="392px" p="20px">
+          <Heading as="h2" size="6" mb="4" weight="bold">
+            {title.toUpperCase()}
+          </Heading>
+          <Text as="p" size="3" weight="regular">
+            {description}
+          </Text>
+        </Box>
+      </Flex>
+      <img className={css.mainImage} src={image.src} alt={image.alt} />
+    </Flex>
+  );
 }
-
-export default AboutItem
